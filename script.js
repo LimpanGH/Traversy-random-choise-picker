9fe7f7125cbdb279a06ab1630f8e98b9d9ebf8f0
LimpanGH <Linusj555@gmail.com>
2023-03-19 13:23:46 +0100

    added to .js and removed from html, now full functioning

--- script.js
+++ script.js
@@ -1,25 +1,69 @@
 const tagsEl = document.getElementById('tags')
 const textarea = document.getElementById('textarea')
 
-textarea.focus()                                        // Automatically puts the cursor in the textarea when the page is loading.
+textarea.focus()
 
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
+
+    if(e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10)
+
+        randomSelect()
+    }
 })
 
 function createTags(input) {
-    const tags = input.split(',').filter(tag => tag.trim()
-    !== '').map(tag => tag.trim())
-
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+    
     tagsEl.innerHTML = ''
 
     tags.forEach(tag => {
         const tagEl = document.createElement('span')
         tagEl.classList.add('tag')
-
+        tagEl.innerText = tag
+        tagsEl.appendChild(tagEl)
     })
-
 }
- 
- 
-Fortsätt: https://www.youtube.com/watch?v=JkeyKeK3V24 timestamp 53 min
+
+function randomSelect() {
+    const times = 30
+
+    const interval = setInterval (() => {
+        const randomTag = pickRandomTag()    
+        
+    if (randomTag !== undefined) {    
+        highlightTag(randomTag)
+    
+        setTimeout(() => {
+            unHighlightTag(randomTag)
+        }, 100)
+    } 
+    }, 100); 
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        }, 100)
+    }, times * 100)
+}
+
+function pickRandomTag() { 
+    const tags = document.querySelectorAll('.tag')
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight')
+}
+
